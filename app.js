6d977e50101d221bc0dc8c074baeba22b8625410
junwoung <565831653@qq.com>
2019-11-07 19:45:33 +0800

    简化入口文件，将路由等功能拆分再整体引入

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+//  引入工具函数
+const utils = require('./business/utils.js')
+global.utils = utils
+
+//  设置session
+// const session = require('./config/session')
+// app.use(session.setSession())
+// app.use(session.flushSession)
+
+// app.set('trust proxy', 1)
+//  判断用户是否登陆
+// var milddlewares = require('./business/middlewares')
+// app.use(milddlewares.isLogin)
+// app.use(milddlewares.isAuthentication)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +34,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//  引入路由
+const routers = require('./routes/index')
+routers.register(app)
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
